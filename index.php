1e98732b37bf5ce0ade00d6f3ef7f2f756fec1b2
cryptstopher <christopherm@inmotionhosting.com>
2017-09-02 10:02:18 -0400

    Deletes some pages and cleans up index page

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php get_header(); ?>
- 
-
-
-       <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-
-
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
    
-     <article>
-     <table>
-    <td><img style="float: left; padding: 0; margin: 17px 10px 1px 0px;" src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/no-money/images/small_globe.gif"></td>
-    <td><h2 style="float: left; margin-bottom: 0;"><a href="<?php echo get_permalink(); ?>"><?php the_title($before = '&#8220;', $after = '&#8221;'); ?></a></h2></td>   
-    </table>
-    <div style="clear:both;"></div>
-    <p style="padding: 0px 0px 0px 0px;"><?php the_excerpt(); ?></p>
+<article>
+<table>
+<td><img style="float: left; padding: 0; margin: 17px 10px 1px 0px;" src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/no-money/images/small_globe.gif"></td>
+<td><h2 style="float: left; margin-bottom: 0;"><a href="<?php echo get_permalink(); ?>"><?php the_title($before = '&#8220;', $after = '&#8221;'); ?></a></h2></td>   
+</table>
+<div style="clear:both;"></div>
+<p style="padding: 0px 0px 0px 0px;">
+    <?php the_excerpt(); ?>
+</p>
     <small><?php the_tags( ); ?></small>
-    </article>
+</article>
 
 
    
