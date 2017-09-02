f50152c760164a0b6c746168f6f6b88f6f4936e9
cryptstopher <stanordan@gmail.com>
2017-09-02 10:06:32 -0400

    Modifies 404 page finally

--- 404.php
+++ 404.php
@@ -1,3 +1,3 @@
-<h1>You done fucked up</h1>
+<h1>Something was wrong...</h1>
 
 <p>The page you are looking for does not exist or is broken or something. Maybe the permalinks are messed up. I don't know.</p>
