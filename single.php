71eff58c107098f93f6cbdcc483887e46885ca12
cryptstopher <16840344+cryptstopher@users.noreply.github.com>
2017-09-25 09:45:51 -0400

    Adds previous and next post links:

--- single.php
+++ single.php
@@ -3,19 +3,27 @@
 
     <div id="the-post">
 	<h1><?php the_title(); ?></h1>
-    <p><?php the_content(); ?></p>
+	<p><?php the_content(); ?></p>
 
-<small>This entry was posted on 
-<?php the_time('l, F jS, Y') ?> at 
-<?php the_time() ?> and is filed 
-under <?php the_category(', ') ?>.</small>
+	<small>This entry was posted on 
+	    <?php the_time('l, F jS, Y') ?> at 
+	    <?php the_time() ?> and is filed 
+	    under <?php the_category(', ') ?>.</small>
     </div>
 
+    
+    <nav class="jilly">
+	<small>
+	    <?php previous_post_link(); ?> | <?php next_post_link(); ?>
+	</small>
+	</nav>
+    
+
 <?php
-    endwhile;
-    else :
-        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-    endif;
-    get_footer(); 
+endwhile;
+else :
+_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+endif;
+get_footer(); 
 ?>
 
