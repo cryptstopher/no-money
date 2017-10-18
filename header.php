e6f10ae3d2c584e9b295ebe1c29d1682d3a1aab5
cryptstopher <16840344+cryptstopher@users.noreply.github.com>
2017-10-18 12:58:14 -0400

    fix gtag thing

--- header.php
+++ header.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-	<?php wp_head(); ?>
-	<title>
-	    <?php
-	    global $page, $paged;
-	    wp_title( '|', true, 'right' );
-	    bloginfo( 'name' );
-	    ?>
-	</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/themes/no-money/style.css">
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79841775-2"></script>
 	<script>
@@ -19,14 +10,22 @@
 
 	 gtag('config', 'UA-79841775-2');
 	</script>
-
-</head>
-<div style="clear:both;"></div> 
-<body <?php body_class(); ?>>
-<header>
-<table>
-<td>
-    <div class="jerry">
+	<?php wp_head(); ?>
+	<title>
+	    <?php
+	    global $page, $paged;
+	    wp_title( '|', true, 'right' );
+	    bloginfo( 'name' );
+	    ?>
+	</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/themes/no-money/style.css">
+    </head>
+    <div style="clear:both;"></div> 
+    <body <?php body_class(); ?>>
+	<header>
+	    <table>
+		<td>
+		    <div class="jerry">
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img style="float:left;margin: 0;" width="237px" height="178px" src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/no-money/images/jerry_computer.gif" style="border:1px solid black;max-width:100%;" alt="Welcome to Computer!"></a>
     </div>
 </td>
