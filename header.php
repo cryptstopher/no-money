f1e23a448aaebad7edd36449da6b78c74c9ba62c
cryptstopher <16840344+cryptstopher@users.noreply.github.com>
2017-10-20 12:36:17 -0400

    Scaled Jerry image finally

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	    <table>
 		<td>
 		    <div class="jerry">
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img style="float:left;margin: 0;" width="237px" height="178px" src="https://media.giphy.com/media/g9501fwdefkXu/giphy.gif" style="border:1px solid black;max-width:100%;" alt="Welcome to Computer!"></a>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img style="float:left;margin: 0;" width="237px" height="178px" src="https://djintendo.files.wordpress.com/2017/10/jerry.gif" style="border:1px solid black;max-width:100%;" alt="Welcome to Computer!"></a>
     </div>
 </td>
 <td>
